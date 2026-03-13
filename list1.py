ed1698abd0d2d9292a3be6c5f5fac134d38c266f
Darko <darkoshypka@gmail.com>
2026-03-13 22:59:20 +0100

    started doing list1

--- list1.py
+++ list1.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 
+# variable
+a = 1
+
+# list of available variables
+print(dir())
+
+# matrix with zeros
+# 2 rows and 3 columns
+print(np.zeros((2,3)))
