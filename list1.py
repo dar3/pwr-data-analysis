534c88ac6b2468389597d9cc2d89e7556cd2e479
Darko <darkoshypka@gmail.com>
2026-03-14 23:00:58 +0100

    test some new functions from numpy

--- list1.py
+++ list1.py
@@ -8,6 +8,17 @@ a = 1
 # list of available variables
 print(dir())
 
-# matrix with zeros
+# matrix filled with zeros
 # 2 rows and 3 columns
 print(np.zeros((2,3)))
+
+# matrix filled with ones
+print(np.ones((2,3)))
+
+# identity matrix
+print("Identity matrix: ")
+print(np.eye(3))
+B = np.eye(5)
+
+# copying matrixes
+print(np.tile(B,(2,3)))
