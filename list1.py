0a694e157a8d4628bb56680ca0697a7af65ba7ef
Darko <darkoshypka@gmail.com>
2026-03-18 23:41:19 +0100

    Exercise 4 and 5 done

--- list1.py
+++ list1.py
@@ -39,10 +39,10 @@ from scipy import stats
 
 # Exercise 2 histogram vs boxplot
 
-# srednia 1, odchylenie 1
+# mean 1, odchylenie 1
 # 100 rows 1 column
 x1 = 2 * (np.random.randn(100, 1) + 1)
-# srednia -1, odchylenie 1
+# mean -1, odchylenie 1
 x2 = 3 * (np.random.randn(100, 1) - 1)
 
 # joining two columns 100 x 2 matrix
@@ -70,7 +70,7 @@ plt.figure(figsize=(12, 10))
 
 plt.subplot(3, 1, 1)
 plt.plot(data)
-plt.title("Line graph (plot)")
+plt.title("Line graph")
 
 plt.subplot(3, 2, 3)
 plt.hist(data, bins=20, color='hotpink', edgecolor='black')
@@ -87,3 +87,96 @@ plt.title("Data boxplot")
 
 plt.tight_layout()
 plt.show()
+
+
+# Exercise 4
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gen1(x, N):
+    m, a, c = 8191, 101, 1731
+    y = np.zeros(N)
+    for i in range(N):
+        x = (a * x + c) % m
+        y[i] = x / m
+    return y
+
+
+def gen2(x, N):
+    a, m, c = 517, 32767, 6923
+    y = np.zeros(N)
+    for i in range(N):
+        x = (a * x + c) % m
+        y[i] = x / m
+    return y
+
+
+def gen3(x, N):
+    c = 65536
+    y = np.zeros(N)
+    for i in range(N):
+        x = (x * 25) % c
+        x = (x * 125) % c
+        y[i] = x / c
+    return y
+
+
+seed = 1
+# samples number
+N = 1100
+
+data_randn = np.random.randn(N)
+data_gen1 = gen1(seed, N)
+data_gen2 = gen2(seed, N)
+data_gen3 = gen3(seed, N)
+
+generators = [data_randn, data_gen1, data_gen2, data_gen3]
+titles = ["(randn)", "Generator  1", "Generator 2", "Generator 3"]
+
+plt.figure(figsize=(12, 8))
+
+for i, data in enumerate(generators):
+    mean = np.mean(data)
+    variance = np.var(data)
+
+    print(f"{titles[i]}: Średnia = {mean:.4f}, Wariancja = {variance:.4f}")
+
+    plt.subplot(2, 2, i + 1)
+    plt.hist(data, bins= 32, color='khaki', edgecolor='white')
+    plt.title(f"{titles[i]}\nMean: {mean:.2f}, Var: {variance:.2f}")
+
+plt.tight_layout()
+plt.show()
+
+# Exercise 5
+
+try:
+    iris_data = np.genfromtxt('data/iris.txt', skip_header=1, usecols=(1, 2, 3, 4))
+    glass_data = np.genfromtxt('data/glass.txt', skip_header=1, usecols=range(0, 9))
+
+
+    iris_attr = iris_data[:, 0]
+    glass_attr = glass_data[:, 1]
+
+    lp = 15
+
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.hist(iris_attr, bins=lp, color='darkorange', edgecolor='black')
+    plt.title(f"Iris - attribute 1 (bins={lp})")
+    plt.xlabel("attribute value")
+
+
+    plt.subplot(1, 2, 2)
+    plt.hist(glass_attr, bins=lp, color='springgreen', edgecolor='black')
+    plt.title(f"Glass - attribute 2 (bins={lp})")
+    plt.xlabel("attribute value")
+
+    plt.tight_layout()
+    plt.show()
+
+except FileNotFoundError:
+    print("File was not found")
